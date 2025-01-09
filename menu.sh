19ee51c3e323f1a221ffe9f7632dc6de5f7a1c42
ketfree <96343945+ketfree@users.noreply.github.com>
2025-01-08 21:04:24 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+clear
 
-# Alias para mostrar el menú
-alias menu='echo "Selecciona una opción:"; echo "1. Instalar Docker"; echo "2. Instalar Theta Edge Node"; read -p "Opción: " opcion; case $opcion in 1) ./docker.sh ;; 2) ./theta.sh ;; *) echo "Opción inválida";; esac'
-
-# Actualizar el sistema
-echo "Actualizando el sistema..."
-apt-get update -y && apt-get upgrade -y
-
-# Mostrar el menú
-menu
+# Función para mostrar el menú
+menu() {
+    echo "Selecciona una opción:"
+    echo "1. Instalar Docker"
+    echo "2. Instalar Theta Edge Node"
+    read -p "Opción: " opcion
+    case $opcion in
+        1) ./docker.sh ;;
+        2) ./theta.sh ;;
+        *) echo "Opción inválida";;
+    esac
+}
