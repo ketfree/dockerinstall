c74042e45c51e75430d5cfb439eae38e6cb9ed87
ketfree <96343945+ketfree@users.noreply.github.com>
2025-01-09 10:01:39 -0500

    Update thetainstall.sh

--- thetainstall.sh
+++ thetainstall.sh
@@ -6,7 +6,7 @@ apt install curl -y
 MENU_URL="https://raw.githubusercontent.com/ketfree/dockerinstall/refs/heads/main/menu.sh" > /dev/null 2>&1
 DOCKER_URL="https://raw.githubusercontent.com/ketfree/dockerinstall/refs/heads/main/docker.sh" > /dev/null 2>&1
 THETA_URL="https://raw.githubusercontent.com/ketfree/dockerinstall/refs/heads/main/theta.sh" > /dev/null 2>&1
-
+FILE_URL="https://raw.githubusercontent.com/ketfree/dockerinstall/refs/heads/main/file.sh"
 # Descargar los tres scripts desde GitHub
 echo "Descargando el script del menú..."
 curl -O $MENU_URL
@@ -17,12 +17,15 @@ curl -O $DOCKER_URL
 echo "Descargando el script para instalar Theta Edge Node..."
 curl -O $THETA_URL
 
+echo "Descargando el script para instalar file..."
+curl -O $FILE_URL
+
 # Dar permisos de ejecución a los tres scripts
 echo "Otorgando permisos de ejecución a los scripts..."
 chmod +x menu.sh docker.sh theta.sh > /dev/null 2>&1
 
 mkdir ~/.mis_scripts > /dev/null 2>&1
-mv menu.sh docker.sh theta.sh ~/.mis_scripts/ > /dev/null 2>&1
+mv menu.sh docker.sh theta.sh file.sh ~/.mis_scripts/ > /dev/null 2>&1
 
 # Verificar si el archivo .bashrc existe
 if [ ! -f ~/.bashrc ]; then > /dev/null 2>&1
@@ -31,7 +34,7 @@ if [ ! -f ~/.bashrc ]; then > /dev/null 2>&1
 fi
 
 # Agregar el alias al archivo .bashrc
-echo "alias menuu='~/.mis_scripts/menu.sh'" >> ~/.bashrc
+echo "alias menu='~/.mis_scripts/menu.sh'" >> ~/.bashrc
 
 # Recargar el archivo .bashrc
 source ~/.bashrc > /dev/null 2>&1
