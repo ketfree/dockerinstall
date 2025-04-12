9b6f111fde8f251a933c41a9781ed84ce05a7e35
ketfree <96343945+ketfree@users.noreply.github.com>
2025-04-12 15:58:14 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -10,7 +10,7 @@ Purple='\033[0;35m'
 Cyan='\033[0;36m'
 White='\033[1;37m'
 
-# Función para mostrar el menú
+# Función para mostrar el menú principal
 menu() {
     clear  # Limpiar la pantalla antes de mostrar el menú
     echo -e "\033[0;36m
@@ -26,7 +26,7 @@ menu() {
     echo -e "\033[0;36m1. \033[0m Instalar Docker \033[0m"
     echo -e "\033[0;36m2. \033[0m Instalar Theta Edge Node \033[0m"
     echo -e "\033[0;36m3. \033[0m Subir la key wallet \033[0m"
-    echo -e "\033[0;36m4. \033[0m ver contenedores \033[0m"
+    echo -e "\033[0;36m4. \033[0m Ver contenedores \033[0m"
     echo -e "\033[0;36m0. \033[0m Salir \033[0m"
     echo -e "\033[1;33m***************************** \033[0m"
     
@@ -41,9 +41,7 @@ menu() {
             ~/.mis_scripts/theta.sh
             ;;
         3)
-            echo -e "${Green}Subiendo la key wallet...${NC}"
-            # Llama al script de subida del archivo
-            ~/.mis_scripts/file.sh
+            submenu_file
             ;;
         4)
             echo -e "${Green}Ejecutando docker ps...${NC}"
@@ -62,9 +60,42 @@ menu() {
     read -p "Presiona Enter para continuar..." dummy
 }
 
-#~/.mis_scripts/menu.sh
+# Función para mostrar el submenú de la opción 3
+submenu_file() {
+    clear
+    echo -e "\033[0;32m#############################"
+    echo -e "\033[0;32m#\033[0m \033[0;34mSubmenú: Subir Key Wallet \033[0m"
+    echo -e "\033[0;32m############################# \033[0m"
+    echo -e "\033[1;33mSelecciona una opción: \033[0m"
+    echo -e "\033[0;36m1. \033[0m Subir key con Nginx \033[0m"
+    echo -e "\033[0;36m2. \033[0m Subir key con file.io \033[0m"
+    echo -e "\033[0;36m0. \033[0m Volver al menú principal \033[0m"
+    echo -e "\033[1;33m***************************** \033[0m"
+    
+    read -p "Opción: " subopcion
+    case $subopcion in
+        1)
+            echo -e "${Green}Subiendo key con Nginx...${NC}"
+            ~/.mis_scripts/file.sh
+            ;;
+        2)
+            echo -e "${Green}Subiendo key con file.io...${NC}"
+            ~/.mis_scripts/file2.sh
+            ;;
+        0)
+            echo -e "${Yellow}Volviendo al menú principal...${NC}"
+            return  # Regresar al menú principal
+            ;;
+        *)
+            echo -e "${Red}Opción inválida${NC}"
+            ;;
+    esac
+    # Esperar antes de mostrar nuevamente el submenú
+    read -p "Presiona Enter para continuar..." dummy
+    submenu_file  # Volver a mostrar el submenú
+}
 
-# Mostrar el menú
+# Mostrar el menú principal
 while true; do
     menu
 done
